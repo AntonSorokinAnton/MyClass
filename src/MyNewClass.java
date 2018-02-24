8406e0e73c272de16be61d2cf85b1f8af45b1597
ASorokinAcer <anton.sorokin@nure.ua>
2018-02-24 16:01:48 +0200

    1

---
+++ src/MyNewClass.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MyNewClass {
+    public static void main(String[] args) {
+
+        for (int i = 0; i<10;i++){
+            System.out.print(i);
+            for (int x = 0; x<10; x++){
+                System.out.println(x);
+            }
+        }
+
+    }
+}
