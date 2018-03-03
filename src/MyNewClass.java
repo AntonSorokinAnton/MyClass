4a141fd5704702526ae1f76ccd020bf3761da7f5
ASorokinAcer <anton.sorokin@nure.ua>
2018-03-03 11:20:46 +0200

    1

--- src/MyNewClass.java
+++ src/MyNewClass.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class MyNewClass {
     public static void main(String[] args) {
 
-        for (int i = 0; i<10;i++){
-            System.out.print(i);
-            for (int x = 0; x<10; x++){
-                System.out.println(x);
+        int [] ourJava = {1,5,3,4,5};
+        int iteration = 0;
+        for (int value : ourJava){
+            if (iteration==ourJava.length-2)
+            {
+                System.out.print(value+". ");
             }
+            else{
+                System.out.print(value+", ");
+            }
+            iteration++;
         }
 
+
+        System.out.println();
     }
 }
